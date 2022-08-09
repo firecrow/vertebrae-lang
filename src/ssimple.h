662fc7a841d010b6a29ea4753a7690d69b0f9c01
firecrow <fire@firecrow.com>
2022-08-09 17:05:40 +0100

    sketch of .h file

---
+++ src/ssimple.h
@@ -0,0 +1,57 @@
+enum TYPE {
+    NONE = 0,
+    CELL, 
+    STRING,
+    SYMBOL
+}
+
+struct string;
+struct tree_node;
+struct tree;
+struct tree_iter;
+struct symbol;
+struct closure;
+struct function;
+struct cell;
+
+struct string {
+    char *content;
+    int length;
+}
+
+struct tree_node {
+    int is_black;
+    void *content;
+}
+
+struct tree {
+    enum TYPE type;
+    struct root *tree_node; 
+    int count;
+}
+
+struct tree_iter {
+    struct tree *tree;
+    struct tree_node *current; 
+}
+
+struct symbol {
+   struct string name; 
+   struct cell *cell;
+}
+
+struct closure {
+    struct parent *closure;
+    struct tree *symbols;
+}
+
+struct function {
+   (*operator)(struct function *self, struct cell); 
+}
+
+struct cell {
+    struct closure *closure;  
+    struct symbol *symbol; // nullable
+    struct function *function; // nullable
+    struct cell *next;
+}
