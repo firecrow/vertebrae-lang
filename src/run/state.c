3e86210df2342a9ae0602673242aa45ec05c246a
firecrow <fire@firecrow.com>
2022-09-11 17:22:37 +0100

    working to have dynamic condition function value returning

--- src/run/state.c
+++ src/run/state.c
@@ -87,6 +87,12 @@ struct crw_state *crw_new_state_context(struct cell* root, struct stack_item *st
    return state;
 }
 
+void start_new_branch(struct crw_state *ctx, struct cell *cell, struct closure *closure){
+    ctx->stack = push_stack(ctx, ctx->cell);
+    ctx->head = setup_new_head(new_head(), cell, closure);
+    ctx->cell = cell;
+}
+
 static void next_step(struct crw_state *ctx){
     if(!ctx->cell){
         fprintf(stderr, "Error next_step called on empty cell\n");
@@ -97,9 +103,7 @@ static void next_step(struct crw_state *ctx){
     bool is_key = crw_process_keys(ctx);
 
     if(ctx->cell->branch){
-        ctx->stack = push_stack(ctx, ctx->cell);
-        ctx->head = setup_new_head(new_head(), ctx->cell->branch, ctx->head->closure);
-        ctx->cell = ctx->cell->branch;
+        start_new_branch(ctx, ctx->cell->branch, ctx->head->closure);
         if(ctx->cell && is_non_head_class(ctx->cell->value)){
             return;
         }
