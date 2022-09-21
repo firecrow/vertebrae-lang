5f5903832064eb0b17d8c22fdcb3e1918a7caeb2
firecrow <fire@firecrow.com>
2022-09-22 00:12:09 +0100

    updating tests

--- test/unit.c
+++ test/unit.c
@@ -421,33 +421,37 @@ int main(){
     test(suite, root->branch->next->value->type == SL_TYPE_KEY, ".hi is a key");
     test(suite, string_cmp(root->branch->next->value->slot.string, str("hi")) == 0, "key name is accurate");
 
-    script = "(print \"the sum is: \" (+ 1 2 3 4) \" units\")";
+    script = "(print \"the sum is: \" (+ 1 2) \" units\")";
 
     root = parse_all(script);
-    printf("\n");
-    print_cell(root->branch);
-    printf("\n");
-    print_cell(root->branch->next);
-    printf("\n");
-    print_cell(root->branch->next->next->branch);
-    printf("\n");
 
-    print_cell(root->branch->next->next->branch->next);
-    printf("\n");
-    print_cell(root->branch->next->next->branch->next->next);
-    printf("\n");
-    print_cell(root->branch->next->next->branch->next->next->next);
-    printf("\n");
-    print_cell(root->branch->next->next->branch->next->next->next->next);
-    printf("\n");
-    print_cell(root->branch->next->next->branch->next->next->next->next->next);
-    printf("\n");
+    cell = root->branch;
+    test(suite, cell->value->type == SL_TYPE_SYMBOL, "print is symbol");
+    test(suite, string_cmp(cell->value->slot.string, str("print")) == 0, "print is the content of the symbol");
 
-    print_cell(root->branch->next->next->branch->next->next->next->next->next->next);
-    printf("\n");
+    cell = root->branch->next;
+    test(suite, cell->value->type == SL_TYPE_STRING, "second is string");
+    test(suite, string_cmp(cell->value->slot.string, str("the sum is: ")) == 0, "string is the content of the string");
+    
+    cell = root->branch->next->next->branch;
+    test(suite, cell->value->type == SL_TYPE_SYMBOL, "+ is symbol");
+    test(suite, string_cmp(cell->value->slot.string, str("+")) == 0, "+ is the content of the symbol");
 
-    print_cell(root->branch->next->next);
-    printf("\n");
+    cell = root->branch->next->next->branch->next;
+    test(suite, cell->value->type == SL_TYPE_INT, "1 is an integer");
+    test(suite, cell->value->slot.integer == 1, "1 is 1");
+
+    cell = root->branch->next->next->branch->next->next;
+    test(suite, cell->value->type == SL_TYPE_INT, "2 is an integer");
+    test(suite, cell->value->slot.integer == 2, "2 is 2");
+
+    cell = root->branch->next->next->branch->next->next->next;
+    test(suite, cell->value == NULL, "next cell value is null");
+    test(suite, cell->next == NULL, "next cell next is null");
+
+    cell = root->branch->next->next;
+    test(suite, cell->value->type == SL_TYPE_STRING, "third section is string");
+    test(suite, string_cmp(cell->value->slot.string, str(" units")) == 0, "string is the content of the string");
 
     summerize(suite);
 
