e3959bda6bb7059ea1e2afd7f8a236132e8b5184
firecrow <fire@firecrow.com>
2022-10-03 21:47:04 +0100

    basic step tests

--- test/unit.c
+++ test/unit.c
@@ -4,7 +4,6 @@
 
 /*
 #include "arithmetic_test.c"
-#include "basic_run_test.c"
 #include "basic_steps.c"
 #include "function_run_test.c"
 #include "function_test.c"
@@ -14,7 +13,6 @@
 #include "unit.c"
 #include "variable_test.c"
 */
-
 
 struct suite *suite = NULL;
 struct order_entry *order_entry = NULL;
@@ -47,49 +45,15 @@ struct cell *eleventh = NULL;
 #include "tree_test.c"
 #include "head_test.c"
 #include "step_test.c"
+#include "basic_run_test.c"
 
 int main(){
     test_tree();
     test_head();
     test_steps();
+    test_basic();
 
 
-    /*************************** test a few next steps ***************/
-    suite = new_suite("Basic step tests");
-
-    root = new_cell(NULL);
-    root->value = new_string_value_obj(str("root"));
-    first = new_cell(NULL);
-    first->value = new_string_value_obj(str("first"));
-    second = new_cell(NULL);
-    second->value = new_string_value_obj(str("second"));
-    third = new_cell(NULL);
-    third->value = new_string_value_obj(str("third"));
-    fourth = new_cell(NULL);
-    fourth->value = new_string_value_obj(str("fourth"));
-
-    root->next = first;
-    first->next = second;
-    second->next = third;
-    third->next = fourth;
-
-    stack = new_stack_item(NULL, root, head);
-    state = crw_new_state_context();
-    crw_setup_state_context(state, root, global);
-
-    state->next(state);
-    test(suite, state->cell == first, "First cell should be current cell after step");
-
-    state->next(state);
-    test(suite, state->cell == second, "Second cell should be current cell after step");
-
-    state->next(state);
-    test(suite, state->cell == third, "Third cell should be current cell after step");
-
-    state->next(state);
-    test(suite, state->cell == fourth, "Fourth cell should be current cell after step");
-    
-    summerize(suite);
 
     /********************************* test pop_stack ********************/
     suite = new_suite("Pop stack branch tests");
