026e0937a8a6487fbf08ec5297bd5a499844ca20
firecrow <fire@firecrow.com>
2022-10-03 21:54:54 +0100

    swap symbol

--- test/unit.c
+++ test/unit.c
@@ -4,11 +4,6 @@
 
 /*
 #include "basic_steps.c"
-#include "function_run_test.c"
-#include "function_test.c"
-#include "parse_test.c"
-#include "swap_symbol_test.c"
-#include "unit.c"
 */
 
 struct suite *suite = NULL;
@@ -22,6 +17,7 @@ struct closure *closure = NULL;
 struct value_obj *value = NULL;
 struct operator_ifc *op;
 struct string *op_name;
+char *script = NULL;
 
 struct closure *global = NULL;
 struct stack_item *stack = NULL;
@@ -46,6 +42,9 @@ struct cell *eleventh = NULL;
 #include "pop_stack.c"
 #include "arithmetic_test.c"
 #include "variable_test.c"
+#include "function_test.c"
+#include "parse_test.c"
+#include "swap_symbol_test.c"
 
 int main(){
     test_tree();
@@ -55,172 +54,13 @@ int main(){
     test_pop_stack();
     test_arithmetic();
     test_variables();
+    test_functions();
+    test_parse();
+    test_swap_symbol();
 
 
 
 
-    
-    /********************************* Function pointer test ********************/
-    suite = new_suite("Function test");
-
-    /*
-    root = new_cell(NULL);
-    first = new_cell(NULL);
-    global = new_closure(NULL);
-
-    root = new_cell(NULL);
-    root->value = new_symbol_value_obj(str("let"));
-    root->id = 1;
-
-    second = new_cell(NULL);
-    second->value = new_key_value_obj(str("show"));
-    second->id = 2;
-
-        fourth = new_cell(NULL);
-        fourth->value = new_symbol_value_obj(str("print"));
-        fourth->id = 4;
-
-        fifth = new_cell(NULL);
-        fifth->value = new_symbol_value_obj(str("message"));
-        fifth->id = 5;
-
-    third = new_cell(NULL);
-    third->id = 3;
-
-    sixth = new_cell(NULL);
-    sixth->id = 6;
-
-    seventh = new_cell(NULL);
-    seventh->value = new_symbol_value_obj(str("show"));
-    seventh->id = 7;
-
-    eigth = new_cell(NULL);
-    eigth->value = new_key_value_obj(str("message"));
-    eigth->id = 8;
-
-    ninth = new_cell(NULL);
-    ninth->value = new_string_value_obj(str("hello, crow-lisp"));
-    ninth->id = 8;
-
-    root->next = first;
-    first->next = second;
-    second->next = third;
-    third->branch = fourth;
-    fourth->next = fifth;
-    third->next = sixth;
-    sixth->branch = seventh;
-    seventh->next = eigth;
-    eigth->next = ninth;
-
-    state = crw_new_state_context();
-    crw_setup_state_context(state, root, global);
-
-    while(state->status != CRW_DONE){
-       state->next(state); 
-    }
-
-    */
-    summerize(suite);
-
-    /********************************* Parse test ********************/
-    suite = new_suite("Parse tests");
-    char *script = "(+ 127)";
-
-    root = parse_all(script);
-
-    /*
-    printf("root: ");
-    print_cell(root);
-    printf("root->branch: ");
-    print_cell(root->branch);
-    printf("root->next: ");
-    print_cell(root->next);
-    printf("\n");
-    */
-
-    test(suite, root->branch->value->type == SL_TYPE_SYMBOL, "+ is symbol");
-    test(suite, string_cmp(root->branch->value->slot.string, str("+")) == 0, "+ is the content of the symbol");
-    test(suite, root->branch->next->value->type == SL_TYPE_INT, "1 is an int");
-    test(suite, root->branch->next->value->slot.integer == 127, "1 is 127");
-
-    script = "(let .hi \"there\")";
-
-    root = parse_all(script);
-
-    test(suite, root->branch->value->type == SL_TYPE_SYMBOL, "let is symbol");
-    test(suite, string_cmp(root->branch->value->slot.string, str("let")) == 0, "let is the content of the symbol");
-    test(suite, root->branch->next->value->type == SL_TYPE_KEY, ".hi is a key");
-    test(suite, string_cmp(root->branch->next->value->slot.string, str("hi")) == 0, "key name is accurate");
-
-    script = "(print \"the sum is: \" (+ 1 2) \" units\")";
-
-    root = parse_all(script);
-
-    cell = root->branch;
-    test(suite, cell->value->type == SL_TYPE_SYMBOL, "print is symbol");
-    test(suite, string_cmp(cell->value->slot.string, str("print")) == 0, "print is the content of the symbol");
-
-    cell = root->branch->next;
-    test(suite, cell->value->type == SL_TYPE_STRING, "second is string");
-    test(suite, string_cmp(cell->value->slot.string, str("the sum is: ")) == 0, "the sum is string is the content of the string");
-    
-    /*
-    printf("%s\n", script);
-    print_cell(root->branch);
-    print_cell(root->branch->next);
-    print_cell(root->branch->next->branch);
-    print_cell(root->branch->next->next);
-    */
-
-    cell = root->branch->next->branch;
-    test(suite, cell->value->type == SL_TYPE_SYMBOL, "+ is symbol");
-    test(suite, string_cmp(cell->value->slot.string, str("+")) == 0, "+ is the content of the symbol");
-
-    cell = root->branch->next->branch->next;
-    test(suite, cell->value->type == SL_TYPE_INT, "1 is an integer");
-    test(suite, cell->value->slot.integer == 1, "1 is 1");
-
-    cell = root->branch->next->branch->next->next;
-    test(suite, cell->value->type == SL_TYPE_INT, "2 is an integer");
-    test(suite, cell->value->slot.integer == 2, "2 is 2");
-
-    cell = root->branch->next->branch->next->next;
-    print_cell(cell);
-    test(suite, cell->next == NULL, "next cell next is null");
-
-    /*
-    printf("root: ");
-    print_cell(root);
-    printf("root->branch: ");
-    print_cell(root->branch);
-    printf("root->branch->next: ");
-    print_cell(root->branch->next);
-    printf("\n");
-    */
-
-    cell = root->branch->next->next;
-    print_cell(root->branch->next);
-    test(suite, cell->value->type == SL_TYPE_STRING, "third section is string");
-    test(suite, string_cmp(cell->value->slot.string, str(" units")) == 0, "units string is the content of the string");
-
-    summerize(suite);
-
-    /********************************* Swap for symbol test ********************/
-    suite = new_suite("Swap for symbol test");
-    struct closure *parent = new_closure(NULL);
-    struct closure *child = new_closure(parent);
-
-    struct value_obj *actual_value = new_int_value_obj(10);
-    struct value_obj *symbol_value = new_symbol_value_obj(str("ten"));
-    tree_add(parent->symbols, str("ten"), actual_value);
-
-    struct value_obj *returned = swap_for_symbol(child, symbol_value);
-
-    test(suite, returned->type == SL_TYPE_INT, "returned is an int");
-    test(suite, returned->slot.integer == 10, "returned is the content of the symbol");
-
-
-    summerize(suite);
     /********************************* Basic run test ********************/
     suite = new_suite("Run tests");
 
