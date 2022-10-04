34644fa07c6cfc433eb327a0c0f44d28f1b810af
firecrow <fire@firecrow.com>
2022-10-04 22:01:25 +0100

    creating save-head function

--- src/library/default_library.c
+++ src/library/default_library.c
@@ -7,6 +7,8 @@ void init_basic_library(struct closure *closure){
     closure_add_value(closure, str("nil"), new_result_value_obj(NIL));
     closure_add_value(closure, str("error"), new_result_value_obj(ERROR));
 
+    closure_add_function(closure, str("save-head"), new_set_head_operator(SAVE_HEAD));
+
     /*----- logic -----*/
     closure_add_function(closure, str("if"), new_condition_operator(CONDITION));
     closure_add_function(closure, str("="), new_equal_operator(EQUAL));
