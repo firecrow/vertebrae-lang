4efbd2dae4ec19fb3681f65c01f4d933ae338190
firecrow <fire@firecrow.com>
2022-11-22 20:34:25 +0000

    working to use one single gt symbol

--- src/parse/into_incr.c
+++ src/parse/into_incr.c
@@ -1,15 +1,8 @@
 int into_incr(struct match_pattern *pattern, struct parse_ctx *ctx, char c){
   if(pattern->state == GKA_PARSE_NOT_STARTED && c == '<'){
-      pattern->state = GKA_PARSE_IN_MATCH;
-      return complete_previous(pattern, ctx);
-  }
-  if(pattern->state == GKA_PARSE_IN_MATCH){
-      if(c == '<'){
-          ctx->next_is_into++;
-          pattern->state = GKA_PARSE_DONE;
-      }else{
-          pattern->state = GKA_PARSE_NOT_STARTED;
-      }
+      pattern->state = GKA_PARSE_DONE;
+      complete_previous(pattern, ctx);
+      ctx->next_is_into++;
       return 1;
   }
   pattern->state = GKA_PARSE_NOT_STARTED;
