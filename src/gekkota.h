066cc9e5a0ac0557ea756a4df40fa652b63820ae
firecrow <fire@firecrow.com>
2022-12-03 16:00:58 +0000

    basic draft compiles

--- src/gekkota.h
+++ src/gekkota.h
@@ -1,10 +1,14 @@
 #include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <regex.h>
 #include <fcntl.h>
 #include <stdarg.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #include "base.h"
 #include "run/run.h"
@@ -14,3 +18,6 @@
 #include "library/library.h"
 #include "parse/parse.h"
 
+#define BUFF_SIZE 4096
+#define STDIN_BLOCK_SIZE 128
+#define INTERVAL 100
--- src/lib/exec.c
+++ src/lib/exec.c
@@ -1,19 +1,17 @@
-#define BUFF_SIZE = 4096;
-#define STDIN_BLOCK_SIZE = 128;
-#define sleep_interval = 100;
+#include "../gekkota.h"
 
 /* TODO: add stdin support, not sure how to implement not blocking */
 
 void handle_out_line(char *line){
-    printf("we got a line '%s'\n", line):
+    printf("we got a line '%s'\n", line);
 }
 
 void handle_err_line(char *line){
-    printf("we got an error line '%s'\n", line):
+    printf("we got an error line '%s'\n", line);
 }
 
 void handle_exit(int exit_code){
-    printf("we got an exit code '%d'\n", exit_code):
+    printf("we got an exit code '%d'\n", exit_code);
 }
 
 /* loop through the bytes of the file until a new lihne is found, or it
@@ -36,7 +34,7 @@ int read_chunk(FILE *stream, char buff[], int *idxp){
                 return 1;
             }
             if(*idxp >= BUFF_SIZE-1){
-                out_buff[*idxp] = 0;
+                buff[*idxp] = 0;
                 return 0;
             }
         }else{
@@ -54,26 +52,36 @@ int read_chunk(FILE *stream, char buff[], int *idxp){
  */
 int exec(char *cmd_path, char *args[]){
     char err_buff[4096];
-    char out_bff[4096];
+    char out_buff[4096];
 
-    int out[];
-    int err[];
-    pipe(in);
+    int out[2];
+    int err[2];
     pipe(out);
-    pipe(err)
+    pipe(err);
+
     int pid = fork();
+    int status;
+    int w;
+    
+    struct timespec interval;
+    interval.tv_sec = 0;
+    interval.tv_nsec = INTERVAL;
+
+    int return_code;
+
+    struct timespec time_remaining;
+
     if(pid == -1){
         fprintf(stderr, "Error forking");
         exit(1);
-    }
-    if(pid == 0){
+    }else if(pid == 0){
         printf("im the child");
 
         dup2(1, out[0]);
         dup2(2, err[0]);
 
         execvp(cmd_path, args);
-    }else if{
+    }else{
         printf("I'm the parent %d", pid);
 
         fcntl(out[1], F_SETFL, O_NONBLOCK);
@@ -88,26 +96,41 @@ int exec(char *cmd_path, char *args[]){
         int out_idx = 0;
         int err_idx = 0;
 
-        /* loop until the process returns */
-        while(1){
+        do {
+            w = waitpid(pid, &status, WUNTRACED | WCONTINUED);
+            if (w == -1) {
+               fprintf(stderr, "Error listening for process status");
+               exit(1);
+            }
+
             /* check for a stdout line */
-            if(reac_chunk(fout, out_buff, &out_idx){
+            if(read_chunk(fout, out_buff, &out_idx)){
                 handle_out_line(out_buff);
             }
             /* check for a stderr line */
-            if(reac_chunk(fout, out_buff, &out_idx){
-                handle_out_line(out_buff);
+            if(read_chunk(ferr, err_buff, &err_idx)){
+                handle_out_line(err_buff);
             }
             /* check if the process has returned */
-            int status_int = waitpid(pid, status, WNOHANG));
+            int status_int = waitpid(pid, &status, WNOHANG);
             if(status_int != -1){
-                if(WEXITED(status)){
+                if(WIFEXITED(status)){
                     WEXITSTATUS(status);
                 }else{
                     break;        
                 }
-            }
-            nanosleep(100);
-        }
+           }
+           if(WIFEXITED(status)){
+               handle_exit(WEXITSTATUS(status));
+               break; 
+           }else if(WIFSIGNALED(status)){
+               handle_exit(WTERMSIG(status));
+               break;
+           }else if(WIFSTOPPED(status)){
+               handle_exit(WSTOPSIG(status));
+               break;
+           }
+           nanosleep(&interval, &time_remaining);
+        }while(!WIFEXITED(status) && !WIFSIGNALED(status));
     }
 }
